4e9c3f127b6595d54af4453b4370fe0e4f03294e
A-Kryston <54906356+A-Kryston@users.noreply.github.com>
2019-09-09 13:51:26 -0400

    Add files via upload

---
+++ Assignment_1_CSV_Census_Data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Andrea Kryston
+# Assignment 1 (Page 7)
+# Load CSV File
+
+import pandas as pd
+Location = "Census_Data_for_Ohio.csv"
+df = pd.read_csv(Location)
+df.head()
+
+
+# In[ ]:
+
+
+
+
