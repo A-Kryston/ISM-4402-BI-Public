65ab340f2d96bb66ccb24c4042e54d74b8f7373a
A-Kryston <54906356+A-Kryston@users.noreply.github.com>
2019-09-09 13:56:31 -0400

    Add files via upload

---
+++ Assignment_1_XLS_Census_Data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Andrea Kryston
+# Assignment 1 (Page 9)
+# Load Excel file
+
+import pandas as pd
+Location = "Census_Data_POP_01.xls"
+df = pd.read_excel(Location)
+df.head()
+
+
+# In[ ]:
+
+
+
+
